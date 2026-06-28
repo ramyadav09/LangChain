91df24d7ca51e60bb31adc7913740fe422301c69
Ram Kumar Yadav <ramyadav2060@gmail.com>
2026-06-28 16:45:56 +0300

    Update dependencies, untrack myenv and vector_store files

--- Vector_Stores/vectorStore.py
+++ Vector_Stores/vectorStore.py
@@ -1,5 +1,5 @@
 from langchain_huggingface import HuggingFaceEmbeddings
-from langchain_community.vectorstores import Chroma
+from langchain_chroma import Chroma
 from langchain_core.documents import Document
 
 # Create documents
